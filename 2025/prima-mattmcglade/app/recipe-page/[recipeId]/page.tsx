6534abb152a473c986112b7928e32908ad770c65
Matt McGlade <matt.mcglade@kurtgeiger.com>
2025-02-12 09:27:17 +0000

    Fix console error

--- 2025/prima-mattmcglade/app/recipe-page/[recipeId]/page.tsx
+++ 2025/prima-mattmcglade/app/recipe-page/[recipeId]/page.tsx
@@ -1,15 +1,15 @@
 import type { Metadata } from 'next'
 import RecipePageLogic from './RecipePageLogic';
 
-export function generateMetadata(params: { params: { id: number } }): Metadata {
-  const orderId = params?.params?.id;
+export function generateMetadata(): Metadata {
   return {
-    title: `Recipe ${orderId}`,
+    title: `Recipe Page`,
   };
 }
 
-export default async function Page(props: { params: { recipeId: number } }) {
-  const recipeId = props?.params?.recipeId;
+export default async function Page(props: { params: { recipeId: string } }) {
+  const { recipeId } = await props.params
+  const idAsNumber = Number(recipeId)
 
-  return <RecipePageLogic recipeId={recipeId} />;
+  return <RecipePageLogic recipeId={idAsNumber} />;
 }
